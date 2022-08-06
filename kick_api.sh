f863c48ce14eac711b90a74a8c6957617958cdf9
Tomoo Morikawa <tmoncm@gmail.com>
2022-08-06 14:38:32 +0900

    add area data

--- kick_api.sh
+++ kick_api.sh
@@ -3,18 +3,21 @@
 # https://api-portal.nhk.or.jp/doc-list-v2-con
 
 # https://api.nhk.or.jp/v2/pg/list/130/{service}/{date}.json?key={apikey}
-baseurl=https://api.nhk.or.jp/v2/pg/list/130
+baseurl=https://api.nhk.or.jp/v2/pg/list/
 dataset=dataset.txt
 mkdir -p json
 export `cat .env`
 touch ${dataset}
 
-for s in `cat etc/service.txt`; do
-    for ((i=7; i < 8; i++)); do
-        sv=`echo ${s} | grep -o "^.."`
-        day=`date -d "${i} day" +'%Y-%m-%d'`
-        url="${baseurl}/${sv}/${day}.json?key=${apikey}"
-        echo "$url"
-        curl $url | jq > "json/${sv}_${day}.json"
+for a in `cat etc/area.txt`; do
+    for s in `cat etc/service.txt`; do
+        for ((i=1; i < 6; i++)); do
+            sv=`echo ${s} | grep -o "^.."`
+            area=`echo ${a} | grep -o "^..."`
+            day=`date -d "${i} day" +'%Y-%m-%d'`
+            url="${baseurl}/${area}/${sv}/${day}.json?key=${apikey}"
+            echo "$url"
+            curl $url | jq > "json/${sv}_${area}_${day}.json"
+        done
     done
 done
