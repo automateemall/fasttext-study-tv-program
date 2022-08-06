f952b2c7d5909d8a5a32392df8a978b2eb598377
Tomoo Morikawa <tmoncm@gmail.com>
2022-08-06 13:39:24 +0900

    add 0813

--- kick_api.sh
+++ kick_api.sh
@@ -10,7 +10,7 @@ export `cat .env`
 touch ${dataset}
 
 for s in `cat etc/service.txt`; do
-    for ((i=1; i < 8; i++)); do
+    for ((i=7; i < 8; i++)); do
         sv=`echo ${s} | grep -o "^.."`
         day=`date -d "${i} day" +'%Y-%m-%d'`
         url="${baseurl}/${sv}/${day}.json?key=${apikey}"
