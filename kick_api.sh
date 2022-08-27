c38c6102099b20b203377e891ae1a7e96576edbe
Tomoo Morikawa <tmoncm@gmail.com>
2022-08-27 11:25:23 +0900

    add new json 2022年 8月 27日 土曜日 11:25:23

--- kick_api.sh
+++ kick_api.sh
@@ -8,7 +8,7 @@ mkdir -p json
 export `cat .env`
 
 for a in `cat sub/area.txt`; do
-    for ((i=0; i < 5; i++)); do
+    for ((i=0; i < 6; i++)); do
         sv=tv
         area=`echo ${a} | grep -o "^..."`
         day=`date -d "${i} day" +'%Y-%m-%d'`
@@ -20,7 +20,7 @@ for a in `cat sub/area.txt`; do
 done
 
 for s in `cat sub/service.txt`; do
-    for ((i=0; i < 5; i++)); do
+    for ((i=0; i < 6; i++)); do
         sv=`echo ${s}`
         area=130
         day=`date -d "${i} day" +'%Y-%m-%d'`
